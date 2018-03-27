3ef5fec94a21f3b4db47c166cd1d49477aa51dcf
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-03-27 10:28:56 +0900

    Implemented BruteCollinearPoints

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,32 +1,41 @@
+import javax.swing.text.Segment;
+import java.util.concurrent.atomic.DoubleAccumulator;
+
 public class BruteCollinearPoints {
 
+    private LineSegment[] segments;
+    private int index;
+
     // finds all line segments containing 4 points
-//    バリデーションの追加から,同値のポイントはslopeの計算から検知できるよ
     public BruteCollinearPoints(Point[] points){
         if (points == null) {
             throw new IllegalArgumentException("points cannot be null");
         }
 
+        segments = new LineSegment[points.length/4];
+        index = 0;
 
         for (int i = 0; i < points.length - 3; i++) {
             Point p1 = points[i];
-
-            if (p1 == null) {
-                throw new IllegalArgumentException("point cannot be null");
-            }
+            checkPoint(p1);
 
             for (int l = i + 1; l < points.length - 2; l++) {
                 Point p2 = points[l];
 
-                if (p2 == null) {
-                    throw new IllegalArgumentException("point cannot be null");
-                }
+                checkPoint(p2);
 
                 Double p1ToP2 = p1.slopeTo(p2);
 
+                checkSlope(p1ToP2);
+
                 for (int m = l + 1; m < points.length - 1; m++) {
                     Point p3 = points[m];
+
+                    checkPoint(p3);
+
                     Double p1ToP3 = p1.slopeTo(p3);
+
+                    checkSlope(p1ToP3);
 
                     if (!p1ToP2.equals(p1ToP3)){
                         continue;
@@ -34,13 +43,19 @@ public class BruteCollinearPoints {
 
                     for (int n = m + 1; n < points.length; n++) {
                         Point p4 = points[n];
+
+                        checkPoint(p4);
+
                         Double p1ToP4 = p1.slopeTo(p4);
+
+                        checkSlope(p1ToP4);
 
                         if (!p1ToP2.equals(p1ToP4)){
                             continue;
                         }
 
-
+                        segments[index] = new LineSegment(p1, p4);
+                        index++;
                     }
                 }
             }
@@ -49,11 +64,23 @@ public class BruteCollinearPoints {
 
     // the number of line segments
     public int numberOfSegments(){
-        return 0;
+        return index;
     }
 
     // the line segments
     public LineSegment[] segments() {
-        return null;
+        return segments;
+    }
+
+    private void checkPoint(Point point) {
+        if (point == null) {
+            throw new IllegalArgumentException("point cannot be null");
+        }
+    }
+
+    private void checkSlope(Double slope) {
+        if (slope == Double.NEGATIVE_INFINITY) {
+            throw new IllegalArgumentException("point cannot be duplicated");
+        }
     }
 }
